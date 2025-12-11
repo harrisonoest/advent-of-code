f8a30a4436c0b91f10eda565aeb168f757085c5a
Harrison Oest <harrison.oest@gmail.com>
2025-12-11 16:49:29 -0700

    feat: added solution for day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,18 @@
 advent_of_code::solution!(1);
 
-fn solve_01(input: &str) -> Option<u64> {
+fn get_first_char(s: &str) -> Option<char> {
+    return s.chars().next();
+}
+
+fn remove_first_char(s: &str) -> &str {
+    return s
+        .char_indices()
+        .nth(1)
+        .and_then(|(i, _)| s.get(i..))
+        .unwrap_or("");
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
     /*
     TODO:
     - Split file into data structure based on newline
@@ -13,18 +25,6 @@ fn solve_01(input: &str) -> Option<u64> {
     - Can we use positive and negative numbers instead of "L" and "R"?
     - Can we handle the addition/subtraction while reading the string?
     */
-    fn get_first_char(s: &str) -> Option<char> {
-        return s.chars().next();
-    }
-
-    fn remove_first_char(s: &str) -> &str {
-        return s
-            .char_indices()
-            .nth(1)
-            .and_then(|(i, _)| s.get(i..))
-            .unwrap_or("");
-    }
-
     let turns = input.split_whitespace();
     let mut current_location = 50;
     let mut count: u64 = 0;
@@ -46,29 +46,48 @@ fn solve_01(input: &str) -> Option<u64> {
             current_location = current_location % 100;
         }
 
-        if current_location < 0 {
+        if current_location.is_negative() {
             current_location = 100 - (current_location % 100).abs();
         }
 
-        println!("Rotating {t}, dial now at position: {current_location}");
+        // println!("Rotating {t}, dial now at position: {current_location}");
 
         if (current_location % 100) == 0 {
             count += 1;
-            println!("Updated count: {count}");
         }
-
     }
-
     // Return the count
     Some(count)
 }
 
-pub fn part_one(input: &str) -> Option<u64> {
-    solve_01(input)
-}
-
 pub fn part_two(input: &str) -> Option<u64> {
-    solve_01(input)
+    /*
+       TODO: Need to count the number of times the dial passes or lands on zero
+
+       Brainstorm: - Get rid of negatives by adding a really large number.
+                   - Mod the result and update the count when it is zero
+    */
+    let turns = input.split_whitespace();
+    let mut count: u64 = 0;
+    let mut current_location = 1000000000 + 50;
+
+    for t in turns {
+        let num_only = remove_first_char(t);
+        let first_letter = get_first_char(t);
+        let num = num_only.parse::<i32>().unwrap();
+
+        for _n in 1..=num {
+            match first_letter {
+                Some('L') => current_location -= 1,
+                _ => current_location += 1,
+            }
+            if current_location % 100 == 0 {
+                count += 1;
+                // println!("Updating count: {count}");
+            }
+        }
+    }
+    Some(count)
 }
 
 #[cfg(test)]
@@ -76,14 +95,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
+    fn test_part_one_example() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(3));
+        let expected = Some(3);
+        println!("Expected: {}", expected.unwrap_or_default());
+        println!("Result: {}", result.unwrap_or_default());
+        assert_eq!(result, expected);
     }
 
     #[test]
-    fn test_part_two() {
+    fn test_part_one_input() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        let expected = Some(1066);
+        println!("Expected: {}", expected.unwrap_or_default());
+        println!("Result: {}", result.unwrap_or_default());
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let expected = Some(6);
+        println!("Expected: {}", expected.unwrap_or_default());
+        println!("Result: {}", result.unwrap_or_default());
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_part_two_input() {
         let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(42));
+        let expected = Some(6223);
+        println!("Expected: {}", expected.unwrap_or_default());
+        println!("Result: {}", result.unwrap_or_default());
+        assert_eq!(result, expected);
     }
 }
