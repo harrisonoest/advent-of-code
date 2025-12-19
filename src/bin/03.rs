09b494a9dde054982945c15dc21703d9a1f55637
Harrison Oest <harrison.oest@gmail.com>
2025-12-19 12:39:17 -0700

    feat: added working solutions for day 3 part 1 and 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,7 +1,6 @@
-use std::cmp::Ordering;
-
 advent_of_code::solution!(3);
 
+// Guesses: 17589 (too high), 17316 (correct)
 pub fn part_one(input: &str) -> Option<u64> {
     let mut ret_val = 0;
     for line in input.lines() {
@@ -10,44 +9,77 @@ pub fn part_one(input: &str) -> Option<u64> {
             .map(|l| l.to_digit(10).unwrap_or_default())
             .collect();
 
-        let first_num_index = line_chars
-            .iter()
-            .enumerate()
-            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
-            .map(|(index, _)| index)
-            .unwrap_or_default();
+        let mut first_num: u32 = 0;
+        let mut second_num: u32 = 0;
+        let mut first_index = 0;
+        let mut second_index;
 
-        // need to add logic for when the highest value is at the last index
-        // of the vector. In this case, we need to find the second highest value and
-        // then use the first value that we found that's at the end
+        // Find first number
+        for i in (1..10).rev() {
+            first_index = line_chars
+                .iter()
+                .position(|&r| r == i)
+                .unwrap_or_else(|| line_chars.len() - 1);
+            if first_index == line_chars.len() - 1 {
+                continue;
+            } else {
+                if first_num < line_chars[first_index] {
+                    first_num = line_chars[first_index];
+                    break;
+                }
+            }
+        }
 
-        let first_num = line_chars[first_num_index];
+        let mut second_half = line_chars.split_off(first_index);
 
-        println!("hhh splitting at index: {first_num_index}");
+        // Throw away the first char
+        second_half.remove(0);
 
-        let second_list = line_chars.split_off(first_num_index);
-
-        let max_second_num_index = second_list
-            .iter()
-            .enumerate()
-            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
-            .map(|(index, _)| index)
-            .unwrap_or_default();
-
-        let second_num = match second_list.len() {
-            0 => 0,
-            _ => second_list[max_second_num_index],
-        };
-
-        println!("first: {first_num}");
-        println!("second: {second_num}");
+        // Find second number
+        for j in (1..10).rev() {
+            second_index = second_half
+                .iter()
+                .position(|&r| r == j)
+                .unwrap_or_else(|| 0);
+            let temp_num = second_half[second_index];
+            if second_num < temp_num {
+                second_num = temp_num;
+            }
+        }
         ret_val += first_num * 10 + second_num;
     }
     Some(ret_val as u64)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+// Guesses: 171741365473332 (correct)
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut ret_val: u64 = 0;
+
+    for line in input.lines() {
+        let line_chars: Vec<u32> = line
+            .chars()
+            .map(|l| l.to_digit(10).unwrap_or_default())
+            .collect();
+
+        let mut stack: Vec<u32> = Vec::new();
+        let mut remove = line_chars.len() - 12;
+
+        for n in line_chars {
+            while remove > 0 && !stack.is_empty() && *stack.last().unwrap() < n {
+                stack.pop();
+                remove -= 1;
+            }
+            stack.push(n);
+        }
+
+        stack.truncate(12);
+
+        let base: u64 = 10;
+        let temp: u64 = stack.iter().fold(0u64, |acc, &d| acc * base + d as u64);
+        ret_val += temp as u64;
+    }
+
+    Some(ret_val as u64)
 }
 
 #[cfg(test)]
@@ -63,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
